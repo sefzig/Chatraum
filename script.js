0b51b7ccab8cb307312400ce2dcb812858c5fec2
Andreas Sefzig <andreas@sefzig.net>
2016-05-29 18:17:18 +0200

    Test

--- script.js
+++ script.js
@@ -28,8 +28,7 @@ module.exports = new Script({
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't teach me how to do anything else!`))
                 .then(() => 'finish');
         }
     }
