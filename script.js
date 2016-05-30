641b72e87364b8725574bfaac6762c3e551e5cdd
Andreas Sefzig <andreas@sefzig.net>
2016-05-30 12:26:34 +0200

    Test

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('[TechnikBot] Hallo, ich bin Cynthia, Technik-Bot.')
-                .then(() => bot.say('[TechnikBot] // Node.js funktioniert.'))
+                .then(() => bot.say('[TechnikBot] _// Node.js funktioniert_'))
                 .then(() => 'askName');
         }
     },
@@ -22,17 +22,17 @@ module.exports = new Script({
             const name = message.text;
             return bot.setProp('name', name)
                 .then(() => bot.say('[TechnikBot] Prima, wir werden Sie von nun an '+name+' nennen.'))
-                .then(() => bot.say('[TechnikBot] // Node.js richtig konfiguriert.'))
+                .then(() => bot.say('[TechnikBot] _// Node.js richtig konfiguriert_'))
                 .then(() => 'testBefehl');
         }
     },
 
     testBefehl: {
-        prompt: (bot) => bot.say('[TechnikBot] Bitte klicken Sie diesen --Befehl!'),
+        prompt: (bot) => bot.say('[TechnikBot] Bitte sagen Sie diesen --Befehl!'),
         receive: (bot, message) => {
             const befehl = message.text;
             return bot.setProp('befehl', befehl)
-                .then(() => bot.say('[TechnikBot] // --Befehle funktionieren.'))
+                .then(() => bot.say('[TechnikBot] _// Befehle funktionieren_'))
                 .then(() => 'testAbgeschlossen');
         }
     },
@@ -41,8 +41,9 @@ module.exports = new Script({
         prompt: (bot) => bot.say('[TechnikBot] Bitte sagen Sie nocheinmal etwas!'),
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say('[TechnikBot] // '+name+', Props funktionieren.'))
-                .then(() => bot.say('[TechnikBot] // Test abgeschlossen.'))
+                .then((name) => bot.say('[TechnikBot] Ich erinnere mich an Sie, '+name+'.'))
+                .then(() => bot.say('[TechnikBot] _// Props funktionieren_'))
+                .then(() => bot.say('[TechnikBot] _// Test abgeschlossen_'))
                 .then(() => bot.say('[TechnikBot] @sefzig, alles läuft!'))
                 .then(() => bot.say('[AndreasSefzig] Danke Cynthia. Viel Spaß :)'))
                 .then(() => 'finish');
